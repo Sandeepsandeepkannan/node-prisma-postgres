e77c2131ebdfed7177cd4f821efe31c4c38b37e8
Sandeep <sandeepsandeepkannan16@gmail.com>
2025-12-14 21:03:53 +0530

    Added the usermodel to prisma schema

---
+++ prisma/migrations/20251214152953_created_user_model/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "age" INTEGER NOT NULL,
+    "city" TEXT NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
